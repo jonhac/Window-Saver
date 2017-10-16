3be1606a2189f4a6ca28bdd2152842a03f21493c
bit-tinker <bit-tinker@posteo.net>
2017-10-16 19:42:31 +0200

    New links in changelog.html; unified looks

--- popup/changelog.js
+++ popup/changelog.js
@@ -2,12 +2,20 @@ document.addEventListener("DOMContentLoaded", init);
 
 let changelog = {
 	1.2: '- Brand new settings page\n- Displaying changelogs\n- Minor style improvements',
-	1.3: '- 1.3 Changes'
+	1.3: '- Some style improvements'
 };
 
 async function init() {
 	document.getElementById('back').addEventListener('click', function() {
 		window.location.href = browser.extension.getURL('popup/popup.html');
+	});
+	document.getElementById('more').addEventListener('click', function() {
+		browser.tabs.create({ url: 'https://addons.mozilla.org/en-US/firefox/addon/window-saver/versions/' });
+		window.close();
+	});
+	document.getElementById('bug').addEventListener('click', function() {
+		browser.tabs.create({ url: 'https://github.com/bit-tinker/Window-Saver/issues' });
+		window.close();
 	});
 
 	let info = await browser.management.getSelf();
