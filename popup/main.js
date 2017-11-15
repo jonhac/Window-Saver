b078cd8fd30848ac7959adde0045b110a93dfdb4
bit-tinker <bit-tinker@posteo.net>
2017-11-15 16:17:19 +0100

    Showing confirmation dialog

--- popup/main.js
+++ popup/main.js
@@ -1,3 +1,7 @@
+let settings = browser.storage.local.get(['folderId', 'confirmSavePrivate'
+	, 'confirmDelete', 'confirmOverride', 'confirmCloseNonPrivate'
+	, 'confirmClosePrivate', 'deleteAfter']);;
+
 window.addEventListener('load', listSaved);
 document.getElementById('save').addEventListener('click', handleSave);
 document.getElementById('name_input').addEventListener('keydown', function(e) {
@@ -19,30 +23,82 @@ async function displayVersion() {
 	document.getElementById('version').innerText = info.version;
 };
 
-async function handleSave() {
-	let settings = await browser.storage.local.get('folderId');
-	let folderId = settings.folderId;
 
-	let name = document.getElementById('name_input').value;
-	if (name === '') {
-		let date = new Date();
-		name = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate()
-			+ ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+let confirmationSender = null;
+let confirmationLocation = null;
+function toggleConfirmation(sender, location, message) {
+	if (confirmationSender !== null) {
+		let confirmed = (confirmationSender === sender);
+		cancelConfirmation();
+		return confirmed;
+	} else {
+		confirmationSender = sender;
+
+		let element = document.createElement('div');
+		element.id = 'confirmation';
+		element.innerText = message;
+
+		confirmationLocation = location;
+		confirmationLocation.replaceWith(element);
+
+		confirmationSender.addEventListener('mouseleave', cancelConfirmation);
+
+		return false;
+	}
+}
+
+function cancelConfirmation() {
+	confirmationSender.removeEventListener('mouseleave', cancelConfirmation);
+
+	let element = document.getElementById('confirmation');
+	element.replaceWith(confirmationLocation);
+
+	confirmationLocation = null;
+	confirmationSender = null;
+}
+
+async function handleSave() {
+
+	let confirmIncognito = (await settings).confirmSavePrivate;
+
+	if (confirmIncognito) {
+		let window = await browser.windows.getCurrent();
+		if (window.incognito) {
+			let confirmed = toggleConfirmation(
+				document.getElementById('save')
+				, document.getElementById('name_input')
+				, 'save private window?');
+			if (confirmed) {
+				save();
+			}
+		} else {
+			save();
+		}
+	} else {
+		save();
 	}
 
-	let windowFolder = await browser.bookmarks.create({ parentId: folderId, index: 0, title: name })
-	saveSession(windowFolder.id);
+	async function save() {
+		let name = document.getElementById('name_input').value;
+		if (name === '') {
+			let date = new Date();
+			name = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate()
+				+ ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+		}
 
-	let dom = buildEntryDom(windowFolder);
-	let bookmarks = document.getElementById('bookmarks');
-	showOrRemoveWelcome();
-	bookmarks.insertBefore(dom, bookmarks.firstChild);
+		let folderId = (await settings).folderId;
+		let windowFolder = await browser.bookmarks.create({ parentId: folderId, index: 0, title: name })
+		saveSession(windowFolder.id);
+
+		let dom = buildEntryDom(windowFolder);
+		let bookmarks = document.getElementById('bookmarks');
+		showOrRemoveWelcome();
+		bookmarks.insertBefore(dom, bookmarks.firstChild);
+	}
 }
 
 async function listSaved() {
-		let settings = await browser.storage.local.get('folderId');
-		let folderId = settings.folderId;
-		let windows = await browser.bookmarks.getChildren(folderId);
+		let windows = await browser.bookmarks.getChildren((await settings).folderId);
 
 		let list = document.getElementById('bookmarks');
 		while(list.firstChild) {
@@ -83,14 +139,14 @@ function buildEntryDom(bookmark) {
 	restoreHereButton.type = 'button';
 	restoreHereButton.className = 'restore_here_button';
 	restoreHereButton.value = '⬆';
-	restoreHereButton.title = 'Close active and open saved window',
+	restoreHereButton.title = 'Close active and open saved window';
 	row.appendChild(restoreHereButton);
 	restoreHereButton.addEventListener('click', handleRestoreHere);
 
 	let overrideButton = document.createElement('input');
 	overrideButton.type = 'button';
 	overrideButton.className = 'override_button';
-	overrideButton.title = 'Override saved with active window',
+	overrideButton.title = 'Override saved with active window';
 	overrideButton.value = '⬇';
 	row.appendChild(overrideButton);
 	overrideButton.addEventListener('click', handleOverride);
@@ -98,7 +154,7 @@ function buildEntryDom(bookmark) {
 	let deleteButton = document.createElement('input');
 	deleteButton.type = 'button';
 	deleteButton.className = 'delete_button';
-	deleteButton.title = 'Delete saved window',
+	deleteButton.title = 'Delete saved window';
 	deleteButton.value = '🗙';
 	row.appendChild(deleteButton);
 	deleteButton.addEventListener('click', handleDelete);
@@ -107,7 +163,7 @@ function buildEntryDom(bookmark) {
 	restoreButton.type = 'button';
 	restoreButton.className = 'restore_button';
 	restoreButton.value = bookmark.title;
-	restoreButton.title = 'Restore saved window',
+	restoreButton.title = 'Restore saved window';
 	row.appendChild(restoreButton);
 	restoreButton.addEventListener('click', handleRestore);
 	
@@ -115,9 +171,31 @@ function buildEntryDom(bookmark) {
 }
 
 async function handleRestoreHere(e) {
-	await handleRestore(e);
+
 	let window = await browser.windows.getCurrent();
-	browser.windows.remove(window.id);
+	let confirm;
+	if (window.incognito) {
+		confirm = (await settings).confirmClosePrivate;
+	} else {
+		confirm = (await settings).confirmCloseNonPrivate;
+	}
+	if (confirm) {
+			let confirmed = toggleConfirmation(
+				this
+				, e.target.nextSibling.nextSibling.nextSibling
+				, 'override?');
+			if (confirmed) {
+				restoreHere();
+			}
+	} else {
+		restoreHere();
+	}
+
+	async function restoreHere() {
+		await handleRestore(e);
+		let window = await browser.windows.getCurrent();
+		browser.windows.remove(window.id);
+	}
 }
 
 function saveSession(folderId) {
@@ -131,22 +209,50 @@ function saveSession(folderId) {
 }
 
 async function handleOverride(e) {
-	let id = e.target.parentNode.id;	
 
-	let oldEntries = await browser.bookmarks.getChildren(id);
-	for(let oldEntry of oldEntries) {
-		browser.bookmarks.remove(oldEntry.id);
+	let confirm = (await settings).confirmOverride;
+	if (confirm) {
+			let confirmed = toggleConfirmation(
+				this
+				, e.target.nextSibling.nextSibling
+				, 'override?');
+			if (confirmed) {
+				override();
+			}
+	} else {
+		override();
 	}
 
-	saveSession(id);
+	async function override() {
+		let id = e.target.parentNode.id;	
+
+		let oldEntries = await browser.bookmarks.getChildren(id);
+		for(let oldEntry of oldEntries) {
+			browser.bookmarks.remove(oldEntry.id);
+		}
+
+		saveSession(id);
+	}
 }
 
-function handleDelete(e) {
-	let id = e.target.parentNode.id;
-
+function deleteBookmarks(id) {
 	browser.bookmarks.removeTree(id);
 	document.getElementById('bookmarks').removeChild(document.getElementById(id));
 	showOrRemoveWelcome();
+}
+
+async function handleDelete(e) {
+	let confirm = (await settings).confirmDelete;
+	let id = e.target.parentNode.id;
+
+	if (confirm) {
+		let confirmed = toggleConfirmation(this, e.target.nextSibling, 'delete?');
+		if (confirmed) {
+			deleteBookmarks(id);
+		}
+	} else {
+		deleteBookmarks(id);
+	}
 }
 async function handleRestore(e) {
 	let id = e.target.parentNode.id;
@@ -172,6 +278,6 @@ async function handleRestore(e) {
 
 	let settings = await browser.storage.local.get('deleteAfter');
 	if (settings.deleteAfter) {
-		handleDelete(e);
+		deleteBookmarks(id);
 	}
 }
